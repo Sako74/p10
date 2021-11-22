a8375560a7feb343cb5cda5e3b41d22f2698a07c
Yann Sako <yann.sakowicz@gmail.com>
2021-11-22 13:09:25 +0100

    Update LUIS scripts

--- luis_create.sh
+++ luis_create.sh
@@ -21,9 +21,6 @@ LUIS_AUTH_ENDPOINT=$(az cognitiveservices account show -n $LUIS_AUTH_NAME -g $LU
 LUIS_PRED_KEY=$(az cognitiveservices account keys list -n $LUIS_PRED_NAME -g $LUIS_RG --query "key1" -o tsv | tr -d '[ \t\n\r\f\v]')
 LUIS_PRED_ENDPOINT=$(az cognitiveservices account show -n $LUIS_PRED_NAME -g $LUIS_RG --query "properties.endpoint" -o tsv | tr -d '[ \t\n\r\f\v]')
 
-# On crée le fichier qui va contenir les infos de connexion aux app LUIS 
-touch $LUIS_ENV_FILE_NAME
-
 # On enregistrer les infos de connexion dans un fichier
 echo LUIS_AUTH_KEY=$LUIS_AUTH_KEY > $LUIS_ENV_FILE_NAME
 echo LUIS_AUTH_ENDPOINT=$LUIS_AUTH_ENDPOINT >> $LUIS_ENV_FILE_NAME
