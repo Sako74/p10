0e861db3203490011ec4d9d9bee0e82e0df95644
Yann Sako <yann.sakowicz@gmail.com>
2021-11-22 11:55:44 +0100

    Update LUIS scripts

--- chatbot_delete.sh
+++ chatbot_delete.sh
@@ -1,8 +1,28 @@
 # On importe les configuration
 source chatbot_config.txt
 
-# On se positionne dans le dossier de l'application
-cd $SITEDIR
+################################################################################
+# Suppression des ressources LUIS
+################################################################################
 
-# On supprime le groupe de ressources de l'application
+# Définition des variables
+SUBSCRIPTION_ID=`az account show --query id --output tsv`
+LUIS_AUTH_ID="/subscriptions/$SUBSCRIPTION_ID/providers/Microsoft.CognitiveServices/locations/$LUIS_LOC/resourceGroups/$LUIS_RG/deletedAccounts/$LUIS_AUTH_NAME"
+LUIS_PRED_ID="/subscriptions/$SUBSCRIPTION_ID/providers/Microsoft.CognitiveServices/locations/$LUIS_LOC/resourceGroups/$LUIS_RG/deletedAccounts/$LUIS_PRED_NAME"
+
+# Suppression du groupe
+az group delete --name $LUIS_RG --yes
+
+# Suppression "hard" du service cognitif (https://stackoverflow.com/a/67938295)
+az resource delete --ids $LUIS_AUTH_ID
+az resource delete --ids $LUIS_PRED_ID
+
+# Suppression des infos de connexion
+rm $LUIS_ENV_FILE_NAME
+
+################################################################################
+# Suppression des ressources du chatbot
+################################################################################
+
+# On supprime le groupe de ressources du chatbot
 az group delete --name $RESOURCEGROUP --yes
