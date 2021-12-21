07aad4a967b3ace50c09b6770d8fe4aa8d5d4752
Yann Sako <yann.sakowicz@gmail.com>
2021-12-21 12:37:20 +0100

    Create Github issue with the data of the alert

--- P10_04_function/create_github_issue/__init__.py
+++ P10_04_function/create_github_issue/__init__.py
@@ -11,47 +11,85 @@ from github import Github
 
 load_dotenv()
 
+# On récupère les variables permettant de créer une issue sur Github
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 GITHUB_REPO = os.getenv("GITHUB_REPO")
 
-g = Github(GITHUB_TOKEN) if GITHUB_TOKEN else None
+def create_issue(issue_title: str, issue_body: str):
+    # On crée le handler Github
+    g = Github(GITHUB_TOKEN)
+
+    # On configure le handler pour le repo du projet
+    repo = g.get_repo(GITHUB_REPO)
+
+    try:
+        # On récupère le tag spécial pour les alertes
+        issue_label_name = "azure alert"
+        issue_label = repo.get_label(issue_label_name)
+    except:
+        # On crée un tag spécial pour les alertes
+        issue_label = repo.create_label(
+            issue_label_name,
+            color="d73a4a",
+            description="Azure alert"
+        )
+    
+    # On crée une issue
+    repo.create_issue(
+        title=issue_title,
+        body=json.dumps(issue_body, indent=2),
+        labels=[issue_label]
+    )
+
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
+    # On spécifie que la réponse sera en JSON
+    func.HttpResponse.mimetype = "application/json"
+    func.HttpResponse.charset = "utf-8"
+
+    try:
+        # On récupère les données
+        data = req.get_json()
+
         try:
-            req_body = req.get_json()
-            logging.info(json.dumps(req_body, indent=2))
-            
-            repo = g.get_repo(GITHUB_REPO)
+            # On initialise les données de l'issue
+            issue_title = data["data"]["essentials"]["description"]
 
-            try:
-                label_name = "insatisfaction alert"
-                label = repo.get_label(label_name)
-            except:
-                logging.info('Create Insatisfaction alert label.')
-
-                label = repo.create_label(
-                    label_name,
-                    color="d73a4a",
-                    description="High insatisfaction rate"
-                )
-            
-            repo.create_issue(
-                title="This is a new issue",
-                body=json.dumps(req_body, indent=2),
-                labels=[label]
+            issue_body = {
+                "start_dt": data["data"]["alertContext"]["condition"]["windowStartTime"],
+                "end_dt": data["data"]["alertContext"]["condition"]["windowEndTime"]
+            }
+        except:
+            # On log les données reçues en cas d'erreur
+            logging.error(
+                "Missing fields 'data.alertContext.condition.windowStartTime'"
+                " and/or 'data.alertContext.condition.windowEndTime' in JSON data:"
             )
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+            logging.error(json.dumps(data))
+
+            # On renvoie l'erreur
+            return func.HttpResponse(json.dumps({"error": "Missing fields 'windowStartTime' and/or 'windowEndTime' in JSON data"}))
+        
+        # On crée l'issue github
+        try:
+            create_issue(issue_title, issue_body)
+        except Exception as e:
+            # On log l'erreur
+            logging.error(e)
+
+            # On renvoie l'erreur
+            return func.HttpResponse(json.dumps({"error": str(e)}))
+
+        # On renvoie les données de l'issue créée
+        return func.HttpResponse(json.dumps({
+            "title": issue_title,
+            "body": issue_body
+        }))
+    except Exception as e:
+        # On log l'erreur
+        logging.error(e)
+
+        # On renvoie l'erreur
+        return func.HttpResponse(json.dumps({"error": str(e)}))
--- P10_04_function/tests/test_create_github_issue.py
+++ P10_04_function/tests/test_create_github_issue.py
@@ -1,28 +1,52 @@
-import unittest
+import unittest, json
+from unittest.mock import patch, MagicMock
 
-import azure.functions as func
-from create_github_issue import main
+from create_github_issue import *
 
 
 class CreateGithubIssueTestCase(unittest.TestCase):
-    NAME = "You"
+    ALERT_DATA = {
+        "data":{
+            "essentials":{
+                "description":"High number of insatifactions"
+            },
+            "alertContext":{
+                "condition":{
+                    "windowStartTime":"2021-12-20T21:25:13Z",
+                    "windowEndTime":"2021-12-20T22:25:13Z"
+                }
+            }
+        }
+    }
 
-    def test_create_github_issue(self):
+    @patch('create_github_issue.create_issue')
+    def test_create_github_issue(self, mock_create_issue):
         # Construct a mock HTTP request.
         req = func.HttpRequest(
-            method='GET',
-            body=None,
+            method='POST',
             url='/api/create_github_issue',
-            params={'name': self.NAME})
+            body=json.dumps(self.ALERT_DATA).encode("utf-8"),
+        )
+
+        expected_data = {
+            "title": self.ALERT_DATA["data"]["essentials"]["description"],
+            "body": {
+                "start_dt": self.ALERT_DATA["data"]["alertContext"]["condition"]["windowStartTime"],
+                "end_dt": self.ALERT_DATA["data"]["alertContext"]["condition"]["windowEndTime"]
+            }
+        }
 
         # Call the function.
-        resp = main(req)
+        resp = main(req).get_body().decode("utf-8")
+        expected_resp = json.dumps(expected_data)
+
+        mock_create_issue.assert_called_once_with(
+            expected_data["title"],
+            expected_data["body"]
+        )
 
         # Check the output.
-        self.assertEqual(
-            resp.get_body().decode("utf-8"),
-            f"Hello, {self.NAME}. This HTTP triggered function executed successfully."
-        )
+        self.assertEqual(resp, expected_resp)
 
 if __name__ == '__main__':
     unittest.main()
